5a700428f5e53bcdb93f5e8a8bee14907060ed99
israelmendez232 <israel.mendez232@gmail.com>
2020-10-13 21:25:13 -0300

    Models into Backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,42 @@
 import express from 'express';
+import './database/connection';
+import { getRepository } from 'typeorm';
+import Orphanages from './models/Orphanages';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/users/:id', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
     return response.json({ message: 'Hello World'});
+});
+
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weeknds
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanages);
+    
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weeknds
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.json({ message: "Orphanage created successfuly.", name: orphanage.name });
 });
 
 app.listen(3333);
