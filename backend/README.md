bb0fc5f8ee0f287e5cf6faaa167317efcdfa05aa
israelmendez232 <israel.mendez232@gmail.com>
2020-10-14 20:44:03 -0300

    Finished with the controllers

--- backend/README.md
+++ backend/README.md
@@ -3,3 +3,6 @@ Rode:
 > npm run dev
 
 Na pasta de `backend`.
+
+Para criar uma migration é:
+> npm run typeorm -- migration:create --name migration_name
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,42 +1,10 @@
 import express from 'express';
 import './database/connection';
-import { getRepository } from 'typeorm';
-import Orphanages from './models/Orphanages';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users/:id', (request, response) => {
-    return response.json({ message: 'Hello World'});
-});
-
-app.post('/orphanages', async (request, response) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weeknds
-    } = request.body;
-
-    const orphanagesRepository = getRepository(Orphanages);
-    
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weeknds
-    });
-
-    await orphanagesRepository.save(orphanage);
-
-    return response.json({ message: "Orphanage created successfuly.", name: orphanage.name });
-});
+app.use(routes);
 
 app.listen(3333);
