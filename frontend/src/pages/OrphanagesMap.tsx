5e610a8c4bfb6fc234e35dc38c7ec3d0688c5fac
israelmendez232 <israel.mendez232@gmail.com>
2020-10-15 19:50:41 -0300

    Conecting secundary pages

--- frontend/src/pages/OrphanagesMap.tsx
+++ frontend/src/pages/OrphanagesMap.tsx
@@ -1,43 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
-import 'leaflet/dist/leaflet.css';
+import mapIcon from "../utils/mapIcon";
 
 function OrphanagesMap() {
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="Happy Web" />
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy Web" />
 
-                    <h2>Escolha um orfanato no mapa.</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+          <h2>Escolha um orfanato no mapa.</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
 
-                <footer>
-                    <strong>Timbó</strong>
-                    <span>Santa Catarina</span>
-                </footer>
-            </aside>
+        <footer>
+          <strong>Timbó</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
 
-            <Map 
-                center={[-26.8271638,-49.2773353]}
-                zoom={15}
-                style={{ width: '100%', height: '100%' }}
-            >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-            </ Map>
+      <Map 
+        center={[-26.8271638,-49.2773353]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
 
-            <Link to="" className="create-orphanage">
-                <FiPlus size={32} color="#FFF" />
-            </Link>
+        <Marker 
+          icon = {mapIcon}
+          position = {[-26.8271638,-49.2773353]}
+        >
+            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                Lar das meninas
+                <Link to="/orphanages/1">
+                    <FiArrowRight size={20} color="#FFF" />
+                </Link>
+            </Popup>
+        </Marker>
+      </ Map>
 
-        </div>
-    )
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+
+    </div>
+  )
 
 }
 
